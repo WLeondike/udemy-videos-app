f9ecbf5d84ef25e6fe6eeac5bc0594caa34ed94c
WLeondike <billyleondike@gmail.com>
2020-11-12 14:20:13 -0500

    VideoItem component appends information and a picture, cleaned up styling

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import VideoItem from './VideoItem';
+import VideoItem from './VideoItem/VideoItem';
 
 const VideoList = ({ videos }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem />;
+    return <VideoItem video={video} />;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className='ui relaxed divided list'>{renderedList}</div>;
 };
 
 export default VideoList;
