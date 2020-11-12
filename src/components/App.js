380faf8b458c7bb84aa94a63976987d10f7a0b66
WLeondike <billyleondike@gmail.com>
2020-11-11 19:13:31 -0500

    hooked up YouTube API to application/can get data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+
+const KEY = 'AIzaSyBlIHwXAreyf82lv9sjkCkoyjcdv8mElbE';
 
 class App extends React.Component {
+    state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const res = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+
+    this.setState({ videos: res.data.items });
+  };
+
   render() {
     return (
-      <div className="ui container">
-        <SearchBar />
+      <div className='ui container'>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos.
       </div>
     );
   }
