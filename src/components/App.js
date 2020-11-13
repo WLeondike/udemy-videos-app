2cadc2d682fa79554b93c3a0d1ff700e423e03d7
WLeondike <billyleondike@gmail.com>
2020-11-13 16:50:46 -0500

    added VideoDetail component, shows title and description

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const KEY = 'AIzaSyBlIHwXAreyf82lv9sjkCkoyjcdv8mElbE';
 
@@ -23,13 +24,14 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log('From the App!', video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
